a58ee62fd96bfee369ab6bea2dfe3c6407c4ba64
mariodacosta <mjaca@iscte-iul.pt>
2019-10-10 10:39:58 +0100

    Estado atual do projeto (ponto e)

---
+++ Avali1ES/src/Engsoftware.java
@@ -0,0 +1,12 @@
+
+public class Engsoftware {
+
+	private String numero;
+	private String grupo;
+	
+	public Engsoftware(String numero, String grupo) {
+		this.numero=numero;
+		this.grupo=grupo;
+	}
+
+}
